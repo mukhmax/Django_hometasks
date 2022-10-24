26df1ad91619d3c41941f53d3511f206c9c484e1
Maksim <mukhmax@gmail.com>
2022-10-24 23:50:15 +0300

    1.2-requests-templates_update

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -19,9 +19,13 @@ def bus_stations(request):
     paginator = Paginator(data, 10)
     page_number = int(request.GET.get('page', 1))
     page = paginator.get_page(page_number)
+    prev_page = paginator.get_page(page_number-1)
+    next_page = paginator.get_page(page_number+1)
 
     context = {
         'bus_stations': data,
         'page': page,
+        'prev_page': prev_page,
+        'next_page': next_page,
     }
     return render(request, 'stations/index.html', context)
